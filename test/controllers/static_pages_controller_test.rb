3faded06d40e110239b4f7eea48c0b6716aff42b
aisowinka <aisowinka@mail.ru>
2022-12-29 14:10:43 +0300

    lab

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
-
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -16,15 +15,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
-
   test "should get about" do
-    get :about
+    get static_pages_about_url
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
-
   test "should get contact" do
-    get :contact
+    get static_pages_contact_url
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
